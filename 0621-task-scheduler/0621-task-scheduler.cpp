03f3a4916ea362f0090bb3ec1855029ebaa17f02
Vinit Kumar <vk6373092@gmail.com>
2026-09-19 20:20:43 +0530

    Time: 0 ms (100%), Space: 38.3 MB (82.98%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,54 +1,23 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        vector<int>freq(26,0);
 
-        vector<int> freq(26, 0);
-
-        for(char ch : tasks) {
-            freq[ch - 'A']++;
+        for(char ch:tasks){
+            freq[ch-'A']++;
         }
-
-        priority_queue<int> pq;
-
-        for(int f : freq) {
-            if(f > 0) {
-                pq.push(f);
-            }
+        int max_freq=0;
+        for(int f:freq){
+            max_freq=max(max_freq,f);
         }
-
-        int time = 0;
-
-        while(!pq.empty()) {
-
-            int cycle = n + 1;
-            vector<int> remaining;
-
-            // One complete cycle
-            while(!pq.empty() && cycle > 0) {
-
-                int f = pq.top();
-                pq.pop();
-
-                f--;
-                time++;
-                cycle--;
-
-                if(f > 0) {
-                    remaining.push_back(f);
-                }
-            }
-
-            // Put tasks back AFTER completing the cycle
-            for(int f : remaining) {
-                pq.push(f);
-            }
-
-            // Remaining positions are idle
-            if(!pq.empty()) {
-                time += cycle;
+        int count=0;
+        for(int f:freq){
+            if(f==max_freq){
+                count++;
             }
         }
+        int time=(max_freq-1)*(n+1)+count;
 
-        return time;
+        return max( (int)tasks.size(),time);
     }
 };

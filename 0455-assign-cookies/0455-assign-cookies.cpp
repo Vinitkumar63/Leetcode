edb586347a9d59e18202a4cd6d2f08516669dd8b
Vinit Kumar <vk6373092@gmail.com>
2026-09-23 18:18:03 +0530

    Time: 13 ms (21.63%), Space: 44.8 MB (47.63%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        int i=0 ,j=0,count=0;
         sort(g.begin(),g.end()); // they are child
         sort(s.begin(),s.end()); // they are cokkie
 
-        int i=0 ,j=0,count=0;
+        
 
         // i is child and j is cokiee
         while(i<g.size()&& j<s.size()){
